c0bc37d4fc973427036d0bbe1c45f28c5a368227
Collin Robison <collinrobison@gmail.com>
2022-04-18 21:10:57 -0500

    added

---
+++ cypress/integration/practice/app.spec.js
@@ -0,0 +1,33 @@
+/* eslint-disable mocha/no-exclusive-tests */
+describe('React TodoMVC', () => {
+  const TODO_ITEM_ONE = 'get egg for trying time'
+  const TODO_ITEM_TWO = 'Pay Rent'
+  const TODO_ITEM_THREE = 'Pickup Dry Cleaning'
+
+  beforeEach(() => {
+    cy.visit('http://localhost:8888')
+  })
+
+  it('adds a single todo', () => {
+    cy.get('.new-todo').type(`${TODO_ITEM_ONE}{enter}`)
+    cy.get('.todo-list li').should('have.length', 1)
+  })
+
+  it('adds three todos', () => {
+    cy.createDefaultTodos().as('todos')
+    cy.get('@todos').should('have.length', 3)
+  })
+
+  it('should append new items to the bottom of the list', () => {
+    cy.createDefaultTodos().as('todos')
+    cy.get('@todos').eq(0).find('label').should('contain', TODO_ITEM_ONE)
+    cy.get('@todos').eq(1).find('label').should('contain', TODO_ITEM_TWO)
+    cy.get('@todos').eq(2).find('label').should('contain', TODO_ITEM_THREE)
+    cy.get('.todo-count').contains('3 items left')
+  })
+
+  it.only('does NOT display the footer when there are no todos', () => {
+    cy.get('.footer').should('not.exist')
+    cy.get('.todo-list').should('not.exist')
+  })
+})
